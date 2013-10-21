b6d741aa13fadbff95cdfc6ed70d7a1daddf402c
Kodi Arfer <git@arfer.net>
2013-10-21 15:38:55 -0400

    Give subjects the task twice, with different payoff matrices.

--- task.py
+++ task.py
@@ -3,6 +3,7 @@
 
 from sys import argv
 from time import sleep
+from random import randint
 from psychopy.event import getKeys, clearEvents
 from psychopy.visual import Rect, Line, TextStim, Circle
 import schizoidpy
@@ -50,18 +51,11 @@ opponent_choice_color = 'yellow'
 choices_by_side = {
     LEFT: COOPERATE, RIGHT: DEFECT,
     UP: COOPERATE, DOWN: DEFECT}
-payoffs = {
-  # The first element of each pair is the player whose payoffs
-  # we're asking about.
-   (DEFECT, COOPERATE): 4,
-   (COOPERATE, COOPERATE): 3,
-   (DEFECT, DEFECT): 2,
-   (COOPERATE, DEFECT): 1}
 
 # Running totals
 def score_counters(player_score, opponent_score):
-    s = ('You:  %4d' % player_score +
-         '\nThem: %4d' % opponent_score)
+    s = ('You: %4d' % player_score +
+         '\nOP:  %4d' % opponent_score)
     return TextStim(o.win, s, color = 'black',
         font = 'monospace',
         pos = (-.5, .7),
@@ -93,7 +87,7 @@ def f():
 opponent_choice_boxes, opponent_choice_marker = f()
 
 # Payoff matrix
-def f():
+def pmatrix(opponent_side, payoffs):
     x_offset = .3
     y_offset = -.2
     length = 1.
@@ -130,6 +124,15 @@ def f():
         fillColor = choice_colors[choices_by_side[side]],
         lineColor = 'black',
         width = cwidth, height = cheight, pos = (x + x_offset, y + y_offset))
+    opponent_marker = Circle(o.win,
+        lineColor = 'black', fillColor = opponent_choice_color,
+        radius = min(cwidth, cheight)/4,
+        pos = (x_offset - length/2 - cwidth/2 - .05,
+            y_offset + opponent_side.sign * length/4))
+    grayout = Rect(o.win,
+        fillColor = 'white', lineColor = None, opacity = .7,
+        width = length, height = length/2,
+        pos = (x_offset, opponent_side.flip.sign * length/4 + y_offset))
     stims = (
         gridsquare(LEFT, UP) + 
         gridsquare(RIGHT, UP) +
@@ -140,54 +143,47 @@ def f():
         o.text(x_offset, length/2 + y_offset + .3, 'You play'),
         card(UP, -length/2 - cwidth/2 - .05, length/4),
         card(DOWN, -length/2 - cwidth/2 - .05, -length/4),
-        o.text(x_offset - length/2 - .2, -.2, 'They\nplay')])
+            o.text(x_offset - length/2 - .2, -.2, 'They\nplay'),
+        grayout,
+        opponent_marker])
     player_marker_f = lambda hside, vside: Rect(o.win,
         fillColor = 'yellow', lineColor = None, opacity = .3,
         width = length/2, height = length/2,
         pos =
            (hside.sign * length/4 + x_offset,
             vside.sign * length/4 + y_offset))
-    opponent_marker_f = lambda vside: Circle(o.win,
-        lineColor = 'black', fillColor = opponent_choice_color,
-        radius = min(cwidth, cheight)/4,
-        pos = (x_offset - length/2 - cwidth/2 - .05, y_offset + vside.sign * length/4))
-    grayout_f = lambda vside: Rect(o.win,
-        fillColor = 'white', lineColor = None, opacity = .7,
-        width = length, height = length/2,
-        pos = (x_offset, vside.sign * length/4 + y_offset))
-    return stims, player_marker_f, opponent_marker_f, grayout_f
-pmatrix, pmatrix_player_marker, pmatrix_opponent_marker, pmatrix_shade = f()
+    return stims, player_marker_f
 
-player_score = 0
-opponent_score = 0
-
-def do_trial(trial):
-    global player_score, opponent_score
-    dkey = ('cooperated', trial)
-    opponent_choice = opponent_choices[trial]
-    opponent_side = UP if choices_by_side[UP] is opponent_choice else DOWN
-    stims = (opponent_choice_boxes + pmatrix + [
-         pmatrix_shade(opponent_side.flip),
-         pmatrix_opponent_marker(opponent_side),
-         opponent_choice_marker(trial)])
-    o.draw(*(stims + [score_counters(player_score, opponent_score)]))
+def wait_for_keypress(dkey, keys):
     with o.timestamps(dkey):
         while True:
-            pressed = getKeys(['escape', 'left', 'right'])
+            pressed = getKeys(keys + ['escape'])
             if 'escape' in pressed:
                 quit()
             if len(pressed) == 1:
-                break
+                return pressed[0]
             clearEvents()
             sleep(.1)
-    chosen_side = LEFT if pressed[0] == 'left' else RIGHT
+
+def do_trial(condition, trial, payoffs):
+    global player_score, opponent_score
+    dkey = ('condition', condition, 'cooperated', trial)
+    opponent_choice = opponent_choices[trial]
+    opponent_side = UP if choices_by_side[UP] is opponent_choice else DOWN
+    pm, pm_player_marker = pmatrix(opponent_side, payoffs)
+    stims = (opponent_choice_boxes + pm +
+        [opponent_choice_marker(trial)])
+    o.draw(*(stims + [score_counters(player_score, opponent_score)]))
+    pressed = wait_for_keypress(dkey, ['left', 'right'])
+
+    chosen_side = LEFT if pressed == 'left' else RIGHT
     player_choice = choices_by_side[chosen_side]
     o.save(dkey, player_choice is COOPERATE)
     player_score += payoffs[player_choice, opponent_choice]
     opponent_score += payoffs[opponent_choice, player_choice]
     o.wait_screen(.5, *(stims + [
         score_counters(player_score, opponent_score),
-        pmatrix_player_marker(chosen_side, opponent_side)]))
+        pm_player_marker(chosen_side, opponent_side)]))
 
 # ------------------------------------------------------------
 # * Preliminaries
@@ -198,14 +194,34 @@ if par['debug']:
 else:
     o.get_subject_id('Decision-Making')
 
+conditions = ['1-2-3-4', '1-2-9-10']
+if randint(0, 1):
+    conditions = conditions[::-1]
+o.save('condition_order', conditions)
+
 o.start_clock()
 
 # ------------------------------------------------------------
 # * The main task
 # ------------------------------------------------------------
 
-for trial in range(len(opponent_choices)):
-    do_trial(trial)
+for condition in conditions:
+    amounts = [int(s) for s in condition.split('-')]
+    payoffs = {
+      # The first element of each pair is the player whose payoffs
+      # we're asking about.
+        (COOPERATE, DEFECT): amounts[0],
+        (DEFECT, DEFECT): amounts[1],
+        (COOPERATE, COOPERATE): amounts[2],
+        (DEFECT, COOPERATE): amounts[3]}
+    player_score = 0
+    opponent_score = 0
+    for trial in range(len(opponent_choices)):
+        do_trial(condition, trial, payoffs)
+    o.draw(score_counters(player_score, opponent_score),
+       o.text(0, 0,
+          'Round complete.\n\nPress the spacebar to continue.'))
+    wait_for_keypress(('condition', condition, 'completion_screen'), ['space'])
 
 # ------------------------------------------------------------
 # * Done
